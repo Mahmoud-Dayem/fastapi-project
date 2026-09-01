64c50accfbd5282a93ae1177f26fc89fe61a13e8
mahmoud <mahmoud.dayeem@gmail.com>
2026-09-01 14:35:00 +0300

    use jinja2

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from posts import posts
 app = FastAPI()
 
 
-tempaltes = Jinja2Templates(directory = "templates")
+templates = Jinja2Templates(directory = "templates")
 
 @app.get("/")
-async def root():
-    return f"<h1>{posts[5]['title']}</h1>"
+async def root(request:Request):
+    response = templates.TemplateResponse(request,"home.html",context={
+        "posts":posts
+    })
+
+    return response
 
 @app.get("/api/posts")
 def get_posts():

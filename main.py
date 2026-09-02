81bb2ddc7fa40f5b0516798288b685829a528a18
mahmoud <mahmoud.dayeem@gmail.com>
2026-09-02 14:26:52 +0300

    commit

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI , Request
 from fastapi.templating import Jinja2Templates
 from posts import posts
-app = FastAPI()
+import os 
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException
+ENV = os.getenv("ENV", "development")
+
+app = FastAPI(
+    docs_url="/docs" if ENV == "development" else None,
+    redoc_url="/redoc" if ENV == "development" else None,
+    openapi_url="/openapi.json" if ENV == "development" else None,
+)
+ 
 
 
 templates = Jinja2Templates(directory = "templates")
@@ -12,10 +23,33 @@ async def root(request:Request):
         "posts":posts,
         'title':"Welcome to Home Page"
     })
-
+    print(request)
     return response
 
 @app.get("/api/posts")
 def get_posts():
     return posts
+ 
+ 
+ 
+
+@app.get("/posts/{post_id}")
+def get_post(post_id: int,   request: Request):
+
+    print(f"user requested post {post_id}")
+
+    for post in posts:
+        if post["id"] == post_id:
+            response = templates.TemplateResponse(request,"post.html",context={
+                "post":post,
+             })
+            print(post)
+            return response
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Post with id {post_id} not found"
+    )
+
+
  
